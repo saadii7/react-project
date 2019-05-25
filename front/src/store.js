3c4901efc4253b7508480a365ab311aec16dcfac
asad <asad4572@gmail.com>
2019-05-25 14:51:22 +0500

    store changes

--- front/src/store.js
+++ front/src/store.js
@@ -9,11 +9,6 @@ const store = createStore(
         inititalState,
         compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()));
         // applyMiddleware(thunk));
-<<<<<<< HEAD
-=======
-
-store.dispatch(fetchAllSports());
->>>>>>> 767c0d00a4935d05906bf63af3b4bc91ac20eee0
 
 
 export default store;
